90bb777635f7b846e23f215ccfbb8affbdd1741e
Adroaldo Neto <adroaldo.neto@zup.com.br>
2022-10-19 12:19:32 -0300

    Chore: Added validation of fields
    Signed-off-by: Adroaldo Neto <adroaldo.neto@zup.com.br>

--- src/main/kotlin/com/stackspot/intellij/ui/toolwindow/panels/PluginInputsPanel.kt
+++ src/main/kotlin/com/stackspot/intellij/ui/toolwindow/panels/PluginInputsPanel.kt
@@ -18,15 +18,27 @@ package com.stackspot.intellij.ui.toolwindow.panels
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.openapi.ui.validation.CHECK_NON_EMPTY
+import com.intellij.openapi.ui.validation.validationTextErrorIf
+import com.intellij.ui.UIBundle
+import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.*
+import com.intellij.ui.layout.ValidationInfoBuilder
 import com.stackspot.model.Input
 import org.apache.commons.lang3.StringUtils
+import javax.swing.JCheckBox
 import javax.swing.JComponent
+
+private const val ENTER_A_NUMBER = "please.enter.a.number"
+private const val AT_LEAST_ONE_MUST_BE_SELECTED = "At least one must be selected"
+private const val INPUT_INVALID_REGEX = "Input is invalid for regex:"
 
 class PluginInputsPanel(
     private val inputs: List<Input>,
     project: Project? = null,
-    val variablesMap: MutableMap<String, Any> = mutableMapOf()
+    val variablesMap: MutableMap<String, Any> = mutableMapOf(),
+    private var checkboxMap: MutableMap<String, Cell<JCheckBox>> = mutableMapOf(),
 ) : DialogWrapper(project, true) {
 
     init {
@@ -35,14 +47,24 @@ class PluginInputsPanel(
     }
 
     override fun createCenterPanel(): JComponent {
-        return panel {
+        val dialogPanel = panel {
             inputs.forEach { input ->
                 draw(input, this)
             }
         }
+
+        val first = checkboxMap.values.first()
+        first.validationOnApply {
+            val isSelected = checkboxMap.values.none { it.component.isSelected }
+            if (isSelected) ValidationInfo(AT_LEAST_ONE_MUST_BE_SELECTED) else null
+        }
+
+        return dialogPanel
     }
 
+
     private fun draw(input: Input, panel: Panel): Row {
+
         return when (input.type) {
             "bool" -> panel.row {
                 checkBox(input.label)
@@ -51,30 +73,61 @@ class PluginInputsPanel(
             }
 
             "int" -> panel.row(input.label) {
-                intTextField()
+                val field = textField()
                     .bindText(functionString(input)) { variablesMap[input.name] = it }
                     .comment(input.help)
+                validatePattern(field, input)
+                field.validationOnInput { validateIfInputIsNumber(it, input) }
+                field.columns(COLUMNS_TINY)
             }
 
             "multiselect" -> panel.row(input.label) {
                 input.items?.forEach { item ->
                     val key = "${input.name}_$item"
-                    checkBox(item)
+                    checkboxMap[key] = checkBox(item)
                         .bindSelected(functionBoolean(input, item, key)) { variablesMap[key] = it }
                         .comment(input.help)
                 }
             }
 
             else -> panel.row(input.label) {
-                input.items?.let { items ->
+                val multipleComboBox = input.items?.let { items ->
                     comboBox(items)
                         .bindItem(functionString(input)) { variablesMap[input.name] = it ?: StringUtils.EMPTY }
                         .comment(input.help)
-                } ?: textField()
-                    .bindText((functionString(input))) { variablesMap[input.name] = it }
-                    .comment(input.help)
+                }
+
+                if (multipleComboBox == null) {
+                    val field = textField()
+                        .bindText((functionString(input))) { variablesMap[input.name] = it }
+                        .comment(input.help)
+                    validatePattern(field, input)
+                }
             }
         }
+    }
+
+    private fun ValidationInfoBuilder.validateIfInputIsNumber(it: JBTextField, input: Input
+    ): ValidationInfo? {
+        val value = it.text
+        val regex = "^[0-9]*\$".toRegex()
+        return when {
+            value == null && input.required -> error(UIBundle.message(ENTER_A_NUMBER))
+            value != null && !regex.matches(value) -> error(UIBundle.message(ENTER_A_NUMBER))
+            else -> null
+        }
+    }
+
+    private fun validatePattern(field: Cell<JBTextField>, input: Input) {
+        val pattern = input.pattern?.toRegex()
+        val checkPattern = pattern?.let {
+            validationTextErrorIf("$INPUT_INVALID_REGEX $pattern") {
+                !pattern.matches(it)
+            }
+        }
+
+        checkPattern?.let { field.textValidation(it) }
+        if (input.required) field.textValidation(CHECK_NON_EMPTY)
     }
 
     private fun functionBoolean(input: Input, item: String, key: String): () -> Boolean = {
